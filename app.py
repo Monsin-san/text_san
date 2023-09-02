e5bde106fe622ddea04599ea684098d7a5d6b105
Monsin-san <56109534+Monsin-san@users.noreply.github.com>
2023-09-02 11:56:17 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+#%%
+'''
+streamlit run D:\GoogleDrive\python\python_code\streamlit_app_2\app.py
+'''
+
+import streamlit as st
+
+st.title("ネコでも使える！テキスト分析（β版）") # タイトル
+st.write("少しずつ機能を追加していきたいと思います。")
+
+# %%
