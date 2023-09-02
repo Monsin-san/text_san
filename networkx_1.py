8eaeca0d1fb6205ab22d979a962b00a4053c8964
Monsin-san <56109534+Monsin-san@users.noreply.github.com>
2023-09-02 16:03:21 +0900

    Update networkx_1.py

--- networkx_1.py
+++ networkx_1.py
@@ -1,16 +1,15 @@
 #%%
 from collections import Counter
 import matplotlib.pyplot as plt
-from sudachipy import Dictionary
 import networkx as nx
-from networkx.algorithms import community
+from networkx.algorithms import community  # networkxのバージョンが2.0以上の場合
+from sudachipy import Dictionary, Tokenizer  # SudachiPyをインポート
 import re
-
-# SudachiPyの辞書を初期化
-dictionary = Dictionary().create()
 
 def make_network(text, frequency_threshold):
     plt.rcParams['font.family'] = 'Meiryo'
+
+    tokenizer = Dictionary().create()  # SudachiPy
 
     # ストップワードの定義
     stop_words = set(["これ", "それ", "あれ", "この", "その", "あの", "ここ", "そこ", "あそこ", "こちら", "どこ", "だれ", "なに", "なん", "何", "私", "こと", "もの"])
@@ -19,14 +18,11 @@ def make_network(text, frequency_threshold):
     text = text.replace("キャッシュ・フロー","キャッシュフロー")
 
     nodes = []
-    tokens = dictionary.tokenize(text)
-
-    for token in tokens:
-        pos = token.part_of_speech()[0]
-        if pos == "名詞":
+    for token in tokenizer.tokenize(text):
+        part_of_speech = token.part_of_speech()[0]
+        if part_of_speech == "名詞":
             word = token.surface()
 
-            # 単語が1文字であるか、数字である場合はスキップ
             if len(word) == 1 or re.match(r'^\d+$', word) or word in stop_words:
                 continue
 
@@ -34,8 +30,6 @@ def make_network(text, frequency_threshold):
 
     bigrams = [(nodes[i], nodes[i + 1]) for i in range(len(nodes) - 1)]
     bigram_counts = Counter(bigrams)
-
-    # 単語の出現頻度をカウント
     node_freq = Counter(nodes)
 
     G = nx.Graph()
@@ -61,16 +55,12 @@ def make_network(text, frequency_threshold):
     pos = nx.spring_layout(G, k=0.5, iterations=50, scale=2.0, seed=30)
     edge_width = [G[u][v]['weight'] * 0.2 for u, v in G.edges()]
     labels = {e: G[e[0]][e[1]]["weight"] for e in G.edges}
-
-    # ノードの大きさを出現頻度に基づいて調整
     node_size = [node_freq[node] * 300 for node in G.nodes()]
 
     fig, ax = plt.subplots(figsize=(16, 16))
-    nx.draw(G, pos, with_labels=True, node_color=node_colors,
-            node_size=node_size, font_size=16, font_family='Meiryo',
-            width=edge_width, cmap=plt.get_cmap("Pastel2"), ax=ax)
+    nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=node_size, font_size=16, font_family='Meiryo', width=edge_width, cmap=plt.get_cmap("Pastel2"), ax=ax)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=12, font_family='Meiryo')
 
     plt.axis('off')
 
-    return fig  # Matplotlibのfigureオブジェクトを返す
+    return fig
