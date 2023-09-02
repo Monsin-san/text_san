407424ee366e787f63345778d4001a9b8b0d6d4f
Monsin-san <56109534+Monsin-san@users.noreply.github.com>
2023-09-02 17:42:42 +0900

    Update networkx_1.py

--- networkx_1.py
+++ networkx_1.py
@@ -1,66 +1,62 @@
 #%%
 from collections import Counter
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import networkx as nx
-from networkx.algorithms import community  # networkxのバージョンが2.0以上の場合
-from sudachipy import Dictionary, Tokenizer  # SudachiPyをインポート
+from networkx.algorithms import community
 import re
+from sudachipy import Dictionary
 
 def make_network(text, frequency_threshold):
-    plt.rcParams['font.family'] = 'Meiryo'
-
-    tokenizer = Dictionary().create()  # SudachiPy
-
-    # ストップワードの定義
+    font = FontProperties(fname="MEIRYO.TTC")
+    
+    tokenizer = Dictionary().create()
+    
     stop_words = set(["これ", "それ", "あれ", "この", "その", "あの", "ここ", "そこ", "あそこ", "こちら", "どこ", "だれ", "なに", "なん", "何", "私", "こと", "もの"])
-
-    text = text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(len(text))}))
+    
+    text = text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
     text = text.replace("キャッシュ・フロー","キャッシュフロー")
-
+    
     nodes = []
     for token in tokenizer.tokenize(text):
-        part_of_speech = token.part_of_speech()[0]
-        if part_of_speech == "名詞":
+        if token.part_of_speech()[0] == "名詞":
             word = token.surface()
-
             if len(word) == 1 or re.match(r'^\d+$', word) or word in stop_words:
                 continue
-
             nodes.append(word)
 
-    bigrams = [(nodes[i], nodes[i + 1]) for i in range(len(nodes) - 1)]
-    bigram_counts = Counter(bigrams)
+    bigram_counts = Counter(zip(nodes, nodes[1:]))
     node_freq = Counter(nodes)
 
     G = nx.Graph()
-
-    for bigram, freq in bigram_counts.items():
+    for (word1, word2), freq in bigram_counts.items():
         if freq >= frequency_threshold:
-            word1, word2 = bigram
             G.add_edge(word1, word2, weight=freq)
-
-    if len(G.edges()) == 0:
+    
+    if not G.edges():
         print("No edges in graph.")
         return None
 
     communities = community.greedy_modularity_communities(G)
-
-    color_map = {}
-    for i, com in enumerate(communities):
-        for node in com:
-            color_map[node] = i
-
+    color_map = {node: i for i, com in enumerate(communities) for node in com}
     node_colors = [color_map.get(node, 0) for node in G.nodes()]
 
     pos = nx.spring_layout(G, k=0.5, iterations=50, scale=2.0, seed=30)
     edge_width = [G[u][v]['weight'] * 0.2 for u, v in G.edges()]
-    labels = {e: G[e[0]][e[1]]["weight"] for e in G.edges}
     node_size = [node_freq[node] * 300 for node in G.nodes()]
 
     fig, ax = plt.subplots(figsize=(16, 16))
-    nx.draw(G, pos, with_labels=True, node_color=node_colors, node_size=node_size, font_size=16, font_family='Meiryo', width=edge_width, cmap=plt.get_cmap("Pastel2"), ax=ax)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=12, font_family='Meiryo')
+    nx.draw(G, pos, with_labels=False, node_color=node_colors, node_size=node_size, width=edge_width, cmap=plt.get_cmap("Pastel2"), ax=ax)
+
+    for node, (x, y) in pos.items():
+        plt.text(x, y, node, fontproperties=font, fontsize=16, ha='center', va='center')
+
+    edge_labels = {e: G[e[0]][e[1]]["weight"] for e in G.edges}
+    for (node1, node2), label in edge_labels.items():
+        x1, y1 = pos[node1]
+        x2, y2 = pos[node2]
+        x, y = (x1 + x2) / 2, (y1 + y2) / 2
+        plt.text(x, y, str(label), fontproperties=font, fontsize=12, ha='center', va='center')
 
     plt.axis('off')
-
     return fig
