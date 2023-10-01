3c21d6956f1346374127e2a6fb29f0de9d199325
Monsin-san <56109534+Monsin-san@users.noreply.github.com>
2023-10-01 13:24:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ image = Image.open("title.png")
 st.image(image,use_column_width=True)
 
 st.write("青山学院大学矢澤研究室では「会計・財務研究におけるテキスト分析」に取り組んでいます。研究活動の一環として、テキスト分析の魅力を体感できるウェブサイトを作成しました。肩の力を抜いてお楽しみください！")
-st.write("最終更新日：2023年9月3日")
+st.write("最終更新日：2023年10月1日")
 st.write("本サイトの特徴")
 st.write("・ネコでもわかるくらい簡単です")
 st.write("・オリジナルのユーザー辞書（2,487単語）およびネガポジ判定にはLoughran-McDonald Dictionaryを使用しています（金先生、伊藤先生に感謝）")
-st.write("・こんな機能があったらいいな、→連絡ください。yazawakenichiagu(at)gmail.com")
+st.write("・こんな機能があったらいいな、→連絡ください。yazawa(at)aoyama.ac.jp")
 
 # %%
 st.title("はじめに")
@@ -42,11 +42,11 @@ st.write("テキストマイニングとは！？")
 
 st.write("テキストマイニングとは、簡単に言えば、大量のテキストデータの中に埋もれている「意味のある情報」を自然言語処理（Natural Language Processing）という技術を用いて取り出すことです。本当はプログラミング言語でコードを書いたりなどちょっと面倒な作業があるのですが、ここではそんな過程をすっとばして、すぱっとさっくりテキスト分析を楽しんでみましょう。")
 
-st.title("準備")
-st.write("下記のボックスに文章を入力してみましょう！サンプルデータを用意していますのでコピー＆ペーストしてください。")
+st.sidebar.title("準備")
+st.sidebar.write("下記のボックスに文章を入力してみましょう！サンプルデータを用意していますのでコピー＆ペーストしてください。")
 
 # ユーザーに文章を入力してもらう
-user_input_text = st.text_area("文章を入力してください:")
+user_input_text = st.sidebar.text_area("文章を入力してください:")
 
 # テキストファイルを読み込む
 with open("sample_A.txt", "r", encoding="utf-8") as f:
@@ -59,8 +59,8 @@ def get_text_download_link(text, filename):
     return f'<a href="data:file/txt;base64,{b64}" download="{filename}">Download text file</a>'
 
 # テキストファイルのダウンロードリンクを作成
-st.write("サンプルテキスト")
-st.markdown(f"【トヨタ自動車　2023年3月期　決算短信】　 {get_text_download_link(text_content, 'sample_A.txt')} ", unsafe_allow_html=True)
+st.sidebar.write("サンプルテキスト")
+st.sidebar.markdown(f"【トヨタ自動車　2023年3月期　決算短信】　 {get_text_download_link(text_content, 'sample_A.txt')} ", unsafe_allow_html=True)
 
 st.title("ステップ１　文字数、単語数、文章数")
 st.write("まずは入力した文章の文字数、単語数、文章数が出力されますので確認してください。文章は長いですか、それとも短いですか？文字数と単語数の割合などどうなっていますか？")
