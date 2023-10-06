3c17262693a9613bbf19c7ed2244c3aeafa15e2d
Monsin-san <56109534+Monsin-san@users.noreply.github.com>
2023-10-06 10:09:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 #%%
-'''
-streamlit run D:\GoogleDrive\python\python_code\streamlit_app_2\app.py
-'''
 import pandas as pd
 import streamlit as st
 from PIL import Image
@@ -21,181 +18,309 @@ from janome.charfilter import *
 from janome.tokenfilter import *
 import os
 from wordcloud_1 import make_wordcloud
+import collections
 
 st.title("ネコでも使える！テキスト分析（β版）") # タイトル
-st.write("少しずつ機能を追加していきたいと思います。")
 
-#image = Image.open("title.png")
 image = Image.open("title.png")
 st.image(image,use_column_width=True)
 
 st.write("青山学院大学矢澤研究室では「会計・財務研究におけるテキスト分析」に取り組んでいます。研究活動の一環として、テキスト分析の魅力を体感できるウェブサイトを作成しました。肩の力を抜いてお楽しみください！")
-st.write("最終更新日：2023年10月1日")
+st.write("最終更新日：2023年10月6日")
 st.write("本サイトの特徴")
-st.write("・ネコでもわかるくらい簡単です")
-st.write("・オリジナルのユーザー辞書（2,487単語）およびネガポジ判定にはLoughran-McDonald Dictionaryを使用しています（金先生、伊藤先生に感謝）")
+st.write("・「コピペ」でテキスト分析ができます。")
+st.write("・会計・財務専門用語を登録したユーザー辞書（2,487単語）およびネガポジ判定にはLoughran-McDonald Dictionary（和訳版）を使用しています（金先生、伊藤先生に感謝）")
 st.write("・こんな機能があったらいいな、→連絡ください。yazawa(at)aoyama.ac.jp")
 
-# %%
 st.title("はじめに")
 st.write("テキストマイニングとは！？")
 
-st.write("テキストマイニングとは、簡単に言えば、大量のテキストデータの中に埋もれている「意味のある情報」を自然言語処理（Natural Language Processing）という技術を用いて取り出すことです。本当はプログラミング言語でコードを書いたりなどちょっと面倒な作業があるのですが、ここではそんな過程をすっとばして、すぱっとさっくりテキスト分析を楽しんでみましょう。")
+st.write("テキストマイニングとは、簡単に言えば、大量のテキストデータの中に埋もれている「意味のある情報」を自然言語処理（Natural Language Processing）という技術を用いて取り出すことです。本当はデータの収集、前処理などちょっと面倒な作業があるのですが、ここではそんな過程をすっとばして、すぱっとさっくりテキスト分析を楽しんでみましょう。")
 
+# サイドバーにテキストを表示
 st.sidebar.title("準備")
-st.sidebar.write("下記のボックスに文章を入力してみましょう！サンプルデータを用意していますのでコピー＆ペーストしてください。")
+st.sidebar.write("下記のボックスに文章を入力してみましょう！サンプルデータを用意していますので、必要に応じてコピー＆ペーストしてください。2社比較したい場合はもう1社入力してください。")
 
-# ユーザーに文章を入力してもらう
-user_input_text = st.sidebar.text_area("文章を入力してください:")
+# サイドバーにユーザーに文章を入力してもらうテキストエリアを配置
+user_input_text_A1 = st.sidebar.text_area("会社名（例：トヨタ自動車）", key='user_input_text_A1')
+user_input_text_A2 = st.sidebar.text_area("文章を入力してください:", key='user_input_text_A2')
 
 # テキストファイルを読み込む
 with open("sample_A.txt", "r", encoding="utf-8") as f:
-    text_content = f.read()
+    text_content_A = f.read()
 
-# テキストファイルをダウンロードするための関数
 def get_text_download_link(text, filename):
     """Generates a link allowing the text to be downloaded"""
     b64 = base64.b64encode(text.encode()).decode()  # 文字をbase64エンコードする
     return f'<a href="data:file/txt;base64,{b64}" download="{filename}">Download text file</a>'
 
-# テキストファイルのダウンロードリンクを作成
-st.sidebar.write("サンプルテキスト")
-st.sidebar.markdown(f"【トヨタ自動車　2023年3月期　決算短信】　 {get_text_download_link(text_content, 'sample_A.txt')} ", unsafe_allow_html=True)
+# サイドバーにテキストファイルのダウンロードリンクを作成
+download_link_A = get_text_download_link(text_content_A, "sample_A.txt")
+st.sidebar.markdown(f"サンプルデータA（トヨタ自動車　2023年3月期　決算短信　MD&A）　 {download_link_A}", unsafe_allow_html=True)
 
+user_input_text_B1 = st.sidebar.text_area("会社名（例：日産自動車）", key='user_input_text_B1')
+user_input_text_B2 = st.sidebar.text_area("文章を入力してください:", key='user_input_text_B2')
+
+# テキストファイルを読み込む
+with open("sample_B.txt", "r", encoding="utf-8") as f:
+    text_content_B = f.read()
+
+# サイドバーにテキストファイルのダウンロードリンクを作成
+download_link_B = get_text_download_link(text_content_B, "sample_B.txt")
+st.sidebar.markdown(f"サンプルデータB（日産自動車　2023年3月期　決算短信　MD&A）　 {download_link_B}", unsafe_allow_html=True)
+
+#%%
+#最初の分析
 st.title("ステップ１　文字数、単語数、文章数")
 st.write("まずは入力した文章の文字数、単語数、文章数が出力されますので確認してください。文章は長いですか、それとも短いですか？文字数と単語数の割合などどうなっていますか？")
 
 # JanomeのTokenizerを初期化
 #tokenizer = Tokenizer()
-tokenizer = Tokenizer(udic='user_dic.csv', udic_enc='utf8')
+tokenizer = Tokenizer(udic="user_dic.csv", udic_enc='utf8')
 
-# テキストが入力された場合の処理
-if user_input_text:
-    user_input_text = user_input_text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
-    user_input_text = user_input_text.replace("キャッシュ・フロー", "キャッシュフロー")
+import pandas as pd
+import streamlit as st
 
-    # 強制的にカウントしたい単語リスト
-    forced_words = []
+# DataFrameを初期化します。
+df = pd.DataFrame(columns=['社名', '文字数', '単語数', '文章数'])
 
-    # 文字数をカウント
-    char_count = len(user_input_text)
-    st.write(f'文字数： {char_count} 字')
+def process_text(user_input_text, company_name):
+    global df
+    if user_input_text:
+        user_input_text = user_input_text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
+        user_input_text = user_input_text.replace("キャッシュ・フロー", "キャッシュフロー")
+        
+        # 文字数をカウント
+        char_count = len(user_input_text)
 
-    # 単語数をカウント
-    tokens = []
-    for token in tokenizer.tokenize(user_input_text):
-        word = token.surface if token.part_of_speech.split(',')[0] != '名詞' else (token.base_form if token.base_form in forced_words else token.surface)
-        tokens.append(word)
-    word_count = len(tokens)
-    st.write(f'単語数： {word_count} 語')
+        # 単語数をカウント
+        tokens = [token.surface for token in tokenizer.tokenize(user_input_text)]
+        word_count = len(tokens)
 
-    # 文章数をカウント
-    sentence_count = len(re.split('[。.!?]', user_input_text)) - 1
-    st.write(f'文章数： {sentence_count} 文')
+        # 文章数をカウント
+        sentence_count = len(re.split('[。.!?]', user_input_text)) - 1
+        
+        # 新しい行を作成し、既存のDataFrameに連結
+        new_row = pd.DataFrame({'社名': [company_name], '文字数': [char_count], '単語数': [word_count], '文章数': [sentence_count]})
+        df = pd.concat([df, new_row], ignore_index=True)
 
+# ユーザー入力テキストを処理します。
+process_text(user_input_text_A2, user_input_text_A1)
+
+# user_input_text_B2が空でない場合、処理を実行します。
+if user_input_text_B2:
+    process_text(user_input_text_B2, user_input_text_B1)
+
+# DataFrameを転置します。
+df_transposed = df.set_index('社名').T
+
+# DataFrameをHTMLテーブルの文字列に変換し、行のインデックスを表示しないようにします。
+html_table = df_transposed.to_html(index_names=False)
+
+# HTMLテーブルをStreamlitで表示します。
+st.markdown(html_table, unsafe_allow_html=True)
+
+#%%
 # Streamlitのインターフェイス
 st.title("ステップ２　単語の出現頻度")
 st.write("続いて単語の出現頻度を分析してみましょう。下のボックスからカウントしたい品詞を選択してください。どのような単語が多く使われているでしょうか？")
 
+def display_pos_frequency(user_input_text, company_name, selected_pos):
+    if user_input_text:
+        pos_freq = count_pos_frequency(user_input_text, selected_pos)
+        st.write(f"{company_name} - {selected_pos}の出現頻度")
+        st.write(pos_freq)
+        
+        # グラフの描画
+        if pos_freq:
+            fig, ax = plt.subplots()
+            
+            # 頻度でソート
+            sorted_pos_freq = pos_freq.most_common(10)
+            words = [item[0] for item in sorted_pos_freq]
+            counts = [item[1] for item in sorted_pos_freq]
+
+            # y軸を逆順にして描画
+            ax.barh(words[::-1], counts[::-1])
+            ax.set_xlabel('出現回数', fontproperties=fontprop)
+            ax.set_title(f"{company_name} - {selected_pos}の出現頻度", fontproperties=fontprop)
+            ax.set_yticklabels(words[::-1], fontproperties=fontprop)
+            plt.tight_layout()
+
+            # Streamlitで表示
+            st.pyplot(fig)
+
 selected_pos = st.selectbox("カウントする品詞を選んでください:", ("名詞", "動詞", "形容詞"), key='my_unique_selectbox_key')
+fontprop = FontProperties(fname="MEIRYO.TTC")
 
-# グラフのフォントを設定
-fontprop = FontProperties(fname="MEIRYO.TTC")  # フォントのパスを適宜変更
+if user_input_text_A2:
+    display_pos_frequency(user_input_text_A2, user_input_text_A1, selected_pos)
 
-if user_input_text:
-    pos_freq = count_pos_frequency(user_input_text, selected_pos)
-    st.write(f'{selected_pos}の出現頻度')
-    st.write(pos_freq)
+if user_input_text_B2:
+    display_pos_frequency(user_input_text_B2, user_input_text_B1, selected_pos)
 
-    # グラフの描画
-    if pos_freq:
-        fig, ax = plt.subplots()
-
-        # 頻度でソート
-        sorted_pos_freq = pos_freq.most_common(10)
-        words = [item[0] for item in sorted_pos_freq]
-        counts = [item[1] for item in sorted_pos_freq]
-
-        # y軸を逆順にして描画
-        ax.barh(words[::-1], counts[::-1])
-
-        ax.set_xlabel('出現回数', fontproperties=fontprop)
-
-        # タイトルにフォントを設定
-        ax.set_title(f'{selected_pos}の出現頻度', fontproperties=fontprop)
-
-        # グラフにフォントを設定して描画
-        ax.set_yticklabels(words[::-1], fontproperties=fontprop)  # y軸のラベルに日本語フォントを設定
-        plt.tight_layout()
-
-        # Streamlitで表示
-        st.pyplot(fig)
-
-user_input = user_input_text
-
+#%%
 st.title("ステップ３　ワードクラウド")
-#st.write("タダイマ開発中デス　m(_ _)m。")
-#ワードクラウド
 st.write("ワードクラウドは単語の出現頻度をイラストにしたもので、どのような単語が多く使われているかを視覚的にわかりやすく表現できます。")
-st.write("下の図は、名詞のみ抽出してワードクラウドを作成しています。なお、場合によっては「連結会計年度」など意味のない単語が大きく描画されることがあります。これらの単語を削除したい場合は下記のストップワードを設定してください。")
+st.write("下の図は、名詞のみ抽出してワードクラウドを作成しています。")
 
-additional_stop_words = st.text_area("追加するストップワードを入力してください（スペースで区切って複数入力可能）").split()
-
-if user_input:
-    wordcloud = make_wordcloud(user_input, additional_stop_words)
+def display_wordcloud(user_input_text, company_name, additional_stop_words):
+    if not user_input_text.strip():  # テキストが空または空白のみの場合
+        st.write(f"{company_name}: テキストが入力されていません。")
+        return
+    
+    wordcloud = make_wordcloud(user_input_text, additional_stop_words)
+    
+    if not wordcloud:  # 単語がない場合
+        st.write(f"{company_name}: 単語が見つかりませんでした。")
+        return
+    
+    st.subheader(f"{company_name}-ワードクラウド")
     fig, ax = plt.subplots(figsize=(15, 12))
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis("off")
     st.pyplot(fig)
 
-#共起ネットワーク
+# A社のストップワード
+additional_stop_words_A = st.text_area("A社の追加ストップワードを入力してください（スペースで区切って複数入力可能）",key='additional_stop_words_A').split()
+
+if user_input_text_A2:
+    display_wordcloud(user_input_text_A2, user_input_text_A1, additional_stop_words_A)
+
+# B社のストップワード
+additional_stop_words_B = st.text_area("B社の追加ストップワードを入力してください（スペースで区切って複数入力可能）",key='additional_stop_words_B').split()
+
+if user_input_text_B2:
+    display_wordcloud(user_input_text_B2, user_input_text_B1, additional_stop_words_B)
+
+#%%
 st.title("ステップ４　共起ネットワーク")
 st.write("共起ネットワークは、単語同士のつながりをイラストにしたもので、どのような単語の組み合わせがみられるかを視覚的にわかりやすく表現できます。")
-st.write("綺麗に図式化するためにはパラメータの細かな調整が必要なのですが、ここでは共起の閾値（＝共起としてカウントする最低値）のみ変更可能にしています。閾値を変更するとどのように変わるのか試してみてください。")
 
-# 1から10までのスライダーを作成。初期値は5。
-slider_value = st.slider('共起の閾値:', min_value=1, max_value=10, value=2)
-
-if user_input:
-    network = make_network(user_input,slider_value)
+def display_network(user_input_text, company_name, slider_value, additional_stop_words):
+    if not user_input_text.strip():  # テキストが空または空白のみの場合
+        st.write(f"{company_name}: テキストが入力されていません。")
+        return
+    
+    network = make_network(user_input_text, slider_value, additional_stop_words)
+    
+    if not network:  # ネットワークが生成できなかった場合
+        st.write(f"{company_name}: 単語の共起ネットワークが見つかりませんでした。")
+        return
+    
+    st.subheader(f"{company_name}-共起ネットワーク")
     st.pyplot(network)
 
+# A社のストップワード
+additional_stop_words_A1 = st.text_area("A社の追加ストップワードを入力してください（スペースで区切って複数入力可能）",key='additional_stop_words_A1').split()
+slider_value_A = st.slider('A社の共起の閾値:', min_value=1, max_value=10, value=2, key='slider_value_A')
+
+if user_input_text_A2:
+    display_network(user_input_text_A2, user_input_text_A1, slider_value_A, additional_stop_words_A1)
+
+# B社のストップワード
+additional_stop_words_B1 = st.text_area("B社の追加ストップワードを入力してください（スペースで区切って複数入力可能）",key='additional_stop_words_B1').split()
+slider_value_B = st.slider('B社の共起の閾値:', min_value=1, max_value=10, value=2, key='slider_value_B1')
+
+if user_input_text_B2:
+    display_network(user_input_text_B2, user_input_text_B1, slider_value_B, additional_stop_words_B)
+
+#%%
 st.write("続いて、文章がどの程度わかりやすいか（＝可読性）、そしてどのようなニュアンス（＝トーン）で書かれているかを判断する指標が算出されます。")
+
 st.title("ステップ５　可読性")
 st.write("タダイマ開発中デス　m(_ _)m。")
 
-#%%
-# トーンスコア
 st.title("ステップ６　トーン")
 st.write("トーンは－1（超ネガティブ）から1（超ポジティブ）で計算されます。0は中立（ニュートラル）となります。 文章のトーンはどのくらいポジティブ（ネガティブ）でしょうか？")
 
-if user_input:
-    score, nofpword, nofnword, top_pwords, top_nwords = tone_score(user_input)
-    evaluation = tone_eval(score)
-    st.write(f"トーンスコア： {score}")
-    st.write(f"トーンレベル： {evaluation}")
+# A社のみの入力がある場合
+if user_input_text_A2 and not user_input_text_B2:
+    score_A, nofpword_A, nofnword_A, top_pwords_A, top_nwords_A = tone_score(user_input_text_A2)
+    evaluation_A = tone_eval(score_A)
+    
+    df_tone_A = pd.DataFrame({
+        user_input_text_A1: [score_A, evaluation_A]
+    }, index=['トーンスコア', 'トーンレベル'])
+    
+    st.table(df_tone_A)
+    
+    df_pwords_A = pd.DataFrame(top_pwords_A, columns=["Positive Word", "Frequency"])
+    df_nwords_A = pd.DataFrame(top_nwords_A, columns=["Negative Word", "Frequency"])
+    
+    st.write(f"{user_input_text_A1} 最も頻繁なポジティブワード:")
+    st.table(df_pwords_A)
+    
+    st.write(f"{user_input_text_A1} 最も頻繁なネガティブワード:")
+    st.table(df_nwords_A)
 
-    # Convert to DataFrame and display as table
-    df_pwords = pd.DataFrame(top_pwords, columns=["Positive Word", "Frequency"])
-    df_nwords = pd.DataFrame(top_nwords, columns=["Negative Word", "Frequency"])
+# B社のみの入力がある場合
+elif user_input_text_B2 and not user_input_text_A2:
+    score_B, nofpword_B, nofnword_B, top_pwords_B, top_nwords_B = tone_score(user_input_text_B2)
+    evaluation_B = tone_eval(score_B)
+    
+    df_tone_B = pd.DataFrame({
+        user_input_text_B1: [score_B, evaluation_B]
+    }, index=['トーンスコア', 'トーンレベル'])
+    
+    st.table(df_tone_B)
+    
+    df_pwords_B = pd.DataFrame(top_pwords_B, columns=["Positive Word", "Frequency"])
+    df_nwords_B = pd.DataFrame(top_nwords_B, columns=["Negative Word", "Frequency"])
+    
+    st.write(f"{user_input_text_B1} 最も頻繁なポジティブワード:")
+    st.table(df_pwords_B)
+    
+    st.write(f"{user_input_text_B1} 最も頻繁なネガティブワード:")
+    st.table(df_nwords_B)
 
+# A社とB社の両方の入力がある場合
+elif user_input_text_A2 and user_input_text_B2:
+    # 上記の両方が入力された場合のコードをここに記述
+    score_A, nofpword_A, nofnword_A, top_pwords_A, top_nwords_A = tone_score(user_input_text_A2)
+    evaluation_A = tone_eval(score_A)
+    
+    score_B, nofpword_B, nofnword_B, top_pwords_B, top_nwords_B = tone_score(user_input_text_B2)
+    evaluation_B = tone_eval(score_B)
+    
+    # トーンスコアとトーンレベルのDataFrameを作成
+    df_tone = pd.DataFrame({
+        user_input_text_A1: [score_A, evaluation_A],
+        user_input_text_B1: [score_B, evaluation_B]
+    }, index=['トーンスコア', 'トーンレベル'])
+    
+    st.table(df_tone)  # トーンスコアとトーンレベルの表を表示
+    
+    df_pwords_A = pd.DataFrame(top_pwords_A, columns=["Positive Word", "Frequency"])
+    df_nwords_A = pd.DataFrame(top_nwords_A, columns=["Negative Word", "Frequency"])
+    
+    df_pwords_B = pd.DataFrame(top_pwords_B, columns=["Positive Word", "Frequency"])
+    df_nwords_B = pd.DataFrame(top_nwords_B, columns=["Negative Word", "Frequency"])
+    
+    # A社とB社のデータフレームを横に結合
+    df_pwords = pd.concat([df_pwords_A.add_prefix(f"{user_input_text_A1} "), df_pwords_B.add_prefix(f"{user_input_text_B1} ")], axis=1)
+    df_nwords = pd.concat([df_nwords_A.add_prefix(f"{user_input_text_A1} "), df_nwords_B.add_prefix(f"{user_input_text_B1} ")], axis=1)
+    
     st.write("最も頻繁なポジティブワード:")
     st.table(df_pwords)
-
+    
     st.write("最も頻繁なネガティブワード:")
     st.table(df_nwords)
-    
+
+# 入力がない場合
+else:
+    st.write("A社またはB社のテキストを入力してください。")
+
+#%%
 # タイトルを設定
 st.title('ステップ７　文章の類似度')
-st.write("下記のボックスに2つの文章を入力すると、文章Aと文章Bが似ているかどうかを計算することができます。類似度は0（まったく似ていない）から1（完全に同じ）で計算されます。 ")
-
-st.write("それでは文章Aには先ほどのテキストデータ（トヨタ自動車2023年3月期）、文章Bには同社の前決算期（2022年3月期）のデータを下記のダウンロードリンクからコピー＆ペーストしてください。 ")
+st.write("２つの文章はどのくらい似ていますか？なぜ似ている（似ていない）のか自分でも考えてみましょう。")
 
 # ユーザーに文章Aを入力してもらう
-text_A = st.text_area("文章Aを入力してください:")
+text_A = user_input_text_A2
 
 # ユーザーに文章Bを入力してもらう
-text_B = st.text_area("文章Bを入力してください:")
+text_B = user_input_text_B2
 
 # テキストファイルを読み込む
 with open("sample_B.txt", "r", encoding="utf-8") as B:
@@ -206,10 +331,6 @@ def get_text_download_link_b(text, filename):
     """Generates a link allowing the text to be downloaded"""
     b64 = base64.b64encode(text.encode()).decode()  # 文字をbase64エンコードする
     return f'<a href="data:file/txt;base64,{b64}" download="{filename}">Download text file</a>'
-
-# テキストファイルのダウンロードリンクを作成
-st.write("サンプルテキスト")
-st.markdown(f"【トヨタ自動車　2022年3月期　決算短信】　 {get_text_download_link(text_content_b, 'sample_B.txt')} ", unsafe_allow_html=True)
 
 # Function to evaluate readability score
 def evaluate_sim(cos_sim):
@@ -234,13 +355,18 @@ if text_A and text_B:
     cos_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
     cos_sim = round(cos_sim,3)
     
-    # 類似度を表示
-    st.write(f'類似度: {cos_sim}')
+    # 類似度とそっくりレベルをDataFrameに保存
     evaluation = evaluate_sim(cos_sim)
-    st.write(f'そっくりレベル: {evaluation}')
+    data = {
+        '類似度': [cos_sim],
+        'そっくりレベル': [evaluation]
+    }
+    df = pd.DataFrame(data)
+    
+    # DataFrameをMarkdown形式で左寄せに表示
+    st.markdown(df.to_markdown(index=False), unsafe_allow_html=True)
 
-st.write("２つの文章はどのくらい似ていますか？なぜ似ている（似ていない）のか自分でも考えてみましょう。")
-
+#%%
 st.title('おわりに')
 st.write("テキスト分析はいかがでしたでしょうか？次はぜひ自分の興味のある文章を入れて結果を確かめてみましょう！")
 
