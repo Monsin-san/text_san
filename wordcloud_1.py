c72793165bbf0581dd8b9dcb37fa0a8bfb105a1c
Monsin-san <56109534+Monsin-san@users.noreply.github.com>
2023-11-11 13:27:38 +0900

    Update wordcloud_1.py

--- wordcloud_1.py
+++ wordcloud_1.py
@@ -11,34 +11,22 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 def make_wordcloud(text, additional_stop_words):
+    if not text.strip():
+        raise ValueError("入力されたテキストが空です。")
+
     stop_words = set(["%)","これ", "それ", "あれ", "この", "その", "あの", "ここ", "そこ", "あそこ", "こちら", "どこ", "だれ", "なに", "なん", "何", "私", "こと", "もの"])
     stop_words.update(additional_stop_words)
 
-    # 強制的にカウントしたい単語リスト（オプション）
-    forced_words = []
-
-    # Janomeで名詞を抽出
     tokenizer = Tokenizer(udic='user_dic.csv', udic_enc='utf8')
-    
     tokens = tokenizer.tokenize(text)
-    nouns = []
-    
-    for token in tokens:
-        pos = token.part_of_speech.split(",")[0]
-        if pos == "名詞":
-            word = token.surface
-            if word not in stop_words and len(word) > 1 and not re.search(r'\d', word):
-                nouns.append(word)
-                
-    # 名詞をスペースで連結
+    nouns = [token.surface for token in tokens if token.part_of_speech.split(",")[0] == "名詞" and token.surface not in stop_words and len(token.surface) > 1 and not re.search(r'\d', token.surface)]
+
+    if not nouns:
+        raise ValueError("テキストから名詞を抽出できませんでした。")
+
     word_nouns = " ".join(nouns)
-
     fpath = "MEIRYO.TTC"
+    wordcloud = WordCloud(font_path=fpath, width=900, height=500, background_color="white", collocations=False, max_words=500, max_font_size=150).generate(word_nouns)
 
-    # WordCloud オブジェクトを生成
-    wordcloud = WordCloud(font_path=fpath, width=900, height=500, 
-                        background_color="white", collocations=False, 
-                        max_words=500, max_font_size=150).generate(word_nouns)
     return wordcloud
-#%%
 
